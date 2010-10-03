61b0c5a1a54570ab6d91a2222abe1dfa0cfc161c
Vladimir Parfinenko <vladimir.parfinenko@gmail.com>
2010-10-03 23:15:47 +0700

    Feature: part list of words

--- learn_words.rb
+++ learn_words.rb
@@ -3,10 +3,52 @@
 require 'rubygems'
 require 'highline/import'
 
-if ARGV.size < 1 or ARGV.size > 2
-  puts "Usage: learn_words.rb FILE [LEARNING_LIMIT]"
+###############################################################
+#               PROCESSING COMMAND LINE OPTIONS
+###############################################################
+
+# TODO: use optparser !!!
+
+USAGE = <<-STR
+Usage: learn_words FILE [--limit N] [--part N/M]
+STR
+
+def exit_with_usage(str = nil)
+  puts USAGE
+  puts("\n" + str) unless str.nil?
   exit 1
 end
+
+limit = 3
+part = 1
+parts_count = 1
+words_file = nil
+
+begin
+  if (i = ARGV.index '--limit')
+    ARGV.delete_at i
+    limit = ARGV.delete_at i
+    raise unless limit =~ /\d+/
+    limit = limit.to_i
+  end
+
+  if (i = ARGV.index '--part')
+    ARGV.delete_at i
+    match = ARGV.delete_at(i).match /(\d+)\/(\d+)/
+    part = match[1].to_i
+    parts_count = match[2].to_i
+    raise if parts_count == 0 or part == 0 or part > parts_count
+  end
+
+  raise if ARGV.count != 1
+  words_file = ARGV.first
+rescue
+  exit_with_usage
+end
+
+###############################################################
+#                        READING WORDS FILE
+###############################################################
 
 class Word
   attr_reader :orig, :trans, :variants_number
@@ -29,10 +71,13 @@ class Word
   end
 
   def asked(answers)
-    right = ( answers.map( &:downcase ).sort == trans.map( &:downcase ).sort )
     @times_asked += 1
-    @times_answered += 1 if right
-    right
+    if answers.map( &:downcase ).sort == trans.map( &:downcase ).sort
+      @times_answered += 1
+      true
+    else
+      false
+    end
   end
 
   def favour
@@ -40,33 +85,65 @@ class Word
   end
 end
 
+
 words = []
-open(ARGV[0]).lines.each do |line|
-  next if line.strip.empty? # pass blank lines
-  parts = line.split( "\t" ).map{ |x| x.strip }.delete_if{ |x| x.empty? }
-  words << Word.new( parts[1], parts[0] ) unless line.start_with? '#'
+begin
+  open(words_file) do |file|
+    file.lines.each do |line|
+      next if line.strip.empty? # pass blank lines
+      parts = line.split( "\t" ).map{ |x| x.strip }.delete_if{ |x| x.empty? }
+      words << Word.new( parts[1], parts[0] ) unless line.start_with? '#'
+    end
+  end
+rescue Exception => e
+  exit_with_usage e.message
 end
 
-LIMIT = (ARGV[1] || 3).to_i
+###############################################################
+#                          PART WORDS
+###############################################################
 
-say %{<%= color('<<<      Welcome to WordLearner !     >>>', GREEN+UNDERLINE) %>}
-say %{<%= color('  made by Vladimir Parfinenko aka cypok  ', GREEN) %>}
-say %{<%= color('   some fixes by Ivan Novikov aka NIA  ', GREEN) %>}
+total_words_count = words.count
+
+# words per part
+wpp = words.count / parts_count
+left = (part-1) * wpp
+right = part * wpp
+if part == parts_count
+  # reminder to last part
+  right = words.count
+end
+words = words[left...right]
+
+###############################################################
+#                        WELCOME MESSAGE
+###############################################################
+
+say %{ <%= color('             Learn Words !           ', GREEN+UNDERLINE) %>}
+say %{ <%= color('made by Vladimir Parfinenko aka cypok', GREEN) %>}
+say %{ <%= color(' some fixes by Ivan Novikov aka NIA  ', GREEN) %>}
 STDOUT.write "\n"
-say %{Current limit of learning is set to #{LIMIT}}
+say %{Current limit of learning is set to #{limit}}
 STDOUT.write "\n"
 say %{Note: if there are more than one variant,}
 say %{separate them by "/" or enter one by one}
 say %{Type "exit" if you want to quit}
 STDOUT.write "\n"
-say %{<%= color('Total #{words.length} words to learn', BLUE) %>}
+say %{<%= color('Total #{total_words_count} words to learn', BLUE) %>}
+if parts_count != 1
+  say %{<%= color('Part #{part} of #{parts_count}: #{words.count} words to learn', BLUE) %>}
+end
 STDOUT.write "\n"
+
+###############################################################
+#                          MAIN LOOP
+###############################################################
 
 answers = []
 word = nil
 while true
-  local_words = words.sort_by { rand }.find_all { |x| x.times_answered < LIMIT }
-  
+  local_words = words.sort_by { rand }.find_all { |x| x.times_answered < limit }
+
   break if local_words.size == 0
 
   min_favour = local_words.map{ |x| x.favour}.min
@@ -80,9 +157,9 @@ while true
     word = local_words.find { |x| x.favour <= (min_favour + d) }
   end
 
-  variants_msg = word.variants_number == 1 ? "" : " (#{word.variants_number} variants)" 
+  variants_msg = word.variants_number == 1 ? "" : " (#{word.variants_number} variants)"
   say %{< <%= color(%q[#{word.orig}], YELLOW) %>#{variants_msg}}
-  
+
   answers = []
   begin
     while answers.length < word.variants_number and not answers.include? "exit"
@@ -106,9 +183,13 @@ while true
   STDOUT.write "\n"
 end
 
+###############################################################
+#                          RESULTS
+###############################################################
+
 STDOUT.write "\n"
 say %{<%= color('Your results:', GREEN) %>}
-words = words.sort do |x, y| 
+words = words.sort do |x, y|
   if x.frequency == y.frequency
     x.trans <=> y.trans
   else
